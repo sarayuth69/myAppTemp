b4f7583e7a3cc584275df43676a19b4e8eee9f93
sarayuth <48449877+sarayuth69@users.noreply.github.com>
2019-10-23 03:20:35 +0700

    123
    132

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { ReactiveFormsModule } from '@angular/forms';
+import * as firebase from 'firebase';
+const config = {
+  apiKey: "AIzaSyBTegoEOP0_fYeSCnuHJbDRN6eZonOQmGs",
+    authDomain: "armevolution-e60b8.firebaseapp.com",
+    databaseURL: "https://armevolution-e60b8.firebaseio.com",
+    projectId: "armevolution-e60b8",
+    storageBucket: "armevolution-e60b8.appspot.com",
+    messagingSenderId: "167921591548",
+    appId: "1:167921591548:web:7edd5104a6e68920"
+};
+firebase.initializeApp(config);
 @NgModule({
-  declarations: [AppComponent],
+ declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    IonicModule.forRoot(),
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,168 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
+import * as firebase from 'firebase';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+   customYearValues = [2020, 2016, 2008, 2004, 2000, 1996];
+  customDayShortNames = ['s\u00f8n', 'man', 'tir', 'ons', 'tor', 'fre', 'l\u00f8r'];
+  customPickerOptions: any;
+public setTemp: any;
+
+public hum:any;
+public tem:any;
+public tempStart:any;
+
+public varlue:any;
+
+public Timer1Starthr : any ;
+public Timer1Startmin : any ;
+public Timer1Stophr : any ;
+public Timer1Stopmin : any ;
+
+
+  constructor(public fb:AngularFireDatabase,
+    public router: Router,) {
+    this.customPickerOptions = {
+      buttons: [{
+        text: 'Save',
+        handler: () => console.log('Clicked Save!')
+      }, {
+        text: 'Log',
+        handler: () => {
+          console.log('Clicked Log. Do not Dismiss.');
+          return false;
+        }
+      }]
+    }
+  }
+ 
+
+  // public setTemp(TempStart){
+  //   console.log(this.TempStart);
+  //   let dt = new Date(TempStart)
+  //   this.fb
+  //       .object("/setTemperature")
+  //       .set(dt.getMinutes()).then(()=>{
+
+  //       });
+  // }
+  ngOnInit(){
+    this.fb
+    .object("/Timer1/Start/hr")
+    .valueChanges()
+    .subscribe((value: any) => {
+      console.log(value);
+      this.Timer1Starthr = value;
+    });
+    this.fb
+    .object("/Timer1/Start/min")
+    .valueChanges()
+    .subscribe((value: any) => {
+      console.log(value);
+      this.Timer1Startmin = value;
+    });
+
+    this.fb
+    .object("/Timer1/Stop/hr")
+    .valueChanges()
+    .subscribe((value: any) => {
+      console.log(value);
+      this.Timer1Stophr = value;
+    });
+    this.fb
+    .object("/Timer1/Stop/min")
+    .valueChanges()
+    .subscribe((value: any) => {
+      console.log(value);
+      this.Timer1Stopmin = value;
+    });
+
+
+     
+      this.fb
+      .object("/humidity")
+      .valueChanges()
+      .subscribe((value: any) => {
+        console.log(value);
+        this.hum = value;
+      });
+
+      this.fb
+      .object("/temperature")
+      .valueChanges()
+      .subscribe((value: any) => {
+        console.log(value);
+        this.tem = value;
+      });
+
+
+
+        // this.fb
+        // .object("/Timer1/Start/min")
+        // .valueChanges()
+        // .subscribe((value: any) => {
+        //   console.log(value);
+        //   this.Timer1Startmin = value;
+        // });
+
+  
+
+
+  }
+
+
+  public getTimeStart1 (timeStart){
+    let dt = new Date(timeStart)
+
+    this.fb
+        .object("/Timer1/Start/hr")
+        .set(dt.getHours())
+        .then(() => {
+        });   
+        
+    this.fb
+        .object("/Timer1/Start/min")
+        .set(dt.getMinutes())
+        .then(() => {
+        });       
+  }
+
+
+
+
+  public getTimeStop1 (timeStop){
+    let dt = new Date(timeStop)
+    this.fb
+    .object("/Timer1/Stop/hr")
+    .set(dt.getHours())
+    .then(() => {
+
+    });   
+ this.fb
+    .object("/Timer1/Stop/min")
+    .set(dt.getMinutes())
+    .then(() => {
+
+    });       
+  }  
+
+  
+  public click(varlue){
+    this.fb
+        .object("/setTemperature/")
+        .set(varlue)
+        .then(() => {
+  
+        }); 
+        console.log(varlue);
+  }
+
 
 }
